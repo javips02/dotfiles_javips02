7a26cfe66c3067c9dab601b7499b87d5ee6fe56f
javips02 <javierperezsalasar@gmail.com>
2024-06-03 18:03:02 +0200

    Primer commit, init.lua con opciones de editor vim incluidas
    Añadidas las opciones para poder ver el cursor centrado, numeros relativos de linea y lugar donde se abren los splits. tambien se ha puesto el clipboard para sincronizarlo con el del sistema

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- Opciones nativas del editor vim
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true 
+vim.opt.splitright = true
+vim.opt.wrap = false -- para que el cursor vaya fluido en archivos con lines largas
+vim.opt.tabstop = 4 -- reducir lo grandes que se ven los tabuladores
+vim.opt.clipboard = "unnamedplus" --sincronizar clipboard con el del sistema
+vim.opt.scrolloff = 999 -- centrar mouse cuando haces scroll
