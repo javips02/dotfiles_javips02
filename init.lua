24336163a962f1bd72ae974f1b49063a223a83a2
javips02 <javierperezsalasar@gmail.com>
2024-06-04 13:07:50 +0200

    Añadido lazy como gestor de paquetes y colorscheme kanagawa a init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
--- Opciones nativas del editor vim
+-- Opciones nativas del editor vim --
+-- ------------------------------- --
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.splitbelow = true 
@@ -13,4 +14,28 @@ vim.opt.inccommand = "split" -- muestra todas las lineas a cambiar juntas en un 
 vim.opt.ignorecase = true -- para que muestre autocompletado de comandos siempre
 vim.opt.termguicolors = true -- para que se muestren más colores n terminales modernas
 
--- Gestor de plugins lazy
+-- Gestor de plugins lazy --
+-- ---------------------- --
+-- Bootstrap (nunca uses variables globales en lua LOCAL súper importante por seguridad y rendimiento)
+-- explicación detallada en apuntes de vim para toda esta instalación
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"  -- coge dir de datos (~/.local/share normalmente) y crea un dir para nvim donde meteremos los plugins instalados
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- inicializamos plugins de lazy
+require("lazy").setup({
+	"rebelot/kanagawa.nvim"
+})
+
+-- Colorscheme --
+-- ----------- --
+vim.cmd.colorscheme("kanagawa-dragon")
