b19ddafcc0087f2b8c0732abff5bce69104f2591
javips02 <javierperezsalasar@gmail.com>
2024-06-06 12:39:29 +0200

    Cambio estructural, incluidos todos los dotfiles para crear repo de dotfiles

---
+++ .config/scripts/updateMacOS.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Permisos de sudo para que no los pida luego
+sudo whoami
+echo "------------------------------"
+echo "COMENZANDO LAS ACTUALIZACIONES"
+echo "------------------------------"
+# Actualizar paquetes de brew
+echo "******************************"
+echo "*    Paquetes de brew:       *"
+echo "******************************"
+#  yabai --stop-service
+/opt/homebrew/bin/brew upgrade
+#  yabai --start-service
+
+#Actualizacion de los macports
+echo "******************************"
+echo "*   Paquetes de Macports:    *"
+echo "******************************"
+sudo port selfupdate
+sudo port upgrade outdated
