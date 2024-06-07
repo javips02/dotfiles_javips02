be882306b57cd9674395ff5496daa123e1fbacf1
javips02 <javierperezsalasar@gmail.com>
2024-06-07 11:53:04 +0200

    Wrote initial bootstrap.sh for macos and changed kitty conf for window size

--- bootstrapMacOs.sh
+++ bootstrapMacOs.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 # Script que instala las dependencias para ejecutar todos los programas de los dotfiles
+# Instalar gestor paquetes homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Instalación de paquetes con brew
+brew install kitty gcc git make fastfetch neovim starship obsidian spotify android-studio whisky canva calibre docker docker-compose colima mactex-no-gui discord brave-browser  
+
