c7a9324db4e22f9f2ff173cf79b5d04e76965a23
javips02 <javierperezsalasar@gmail.com>
2024-06-21 11:46:38 +0200

    Modificado bootstrap para añadir stow y preguntar al usuario si quiere hacer stow del directorio de dotfiles

--- bootstrapMacOs.sh
+++ bootstrapMacOs.sh
@@ -4,5 +4,20 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Instalación de paquetes con brew
-brew install kitty gcc git make fastfetch neovim starship obsidian spotify android-studio whisky canva calibre docker docker-compose colima mactex-no-gui discord brave-browser megasync arc
+brew install kitty gcc git make fastfetch neovim starship obsidian spotify android-studio whisky canva calibre docker docker-compose colima mactex-no-gui discord brave-browser megasync arc stow tldr
+
+# Poner los dotfiles en su sitio con gnu stow
+# Simulación del comando Stow
+echo "Simulando la aplicación de configuraciones con Stow:"
+stow --simulate -R --override="--force" .
+# Preguntar al usuario si quiere proceder con la operación real
+read -p "¿Quieres proceder con la aplicación real de las configuraciones? (s/n): " respuesta
+if [[ "$respuesta" == "s" ]]; then
+    # Aplicación real del comando Stow
+    echo "Aplicando configuraciones con Stow:"
+    stow -R --override="--force" .
+    echo "Configuraciones aplicadas con éxito."
+else
+    echo "Operación cancelada por el usuario."
+fi
 
