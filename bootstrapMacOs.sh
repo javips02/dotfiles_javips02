4a5992188dcb0a1196efe95ce25d9226e77475d6
javips02 <javierperezsalasar@gmail.com>
2024-06-24 20:03:52 +0200

    Fixed bootstrap, now installs all dependencies
    Setup of bashrc lke files and paths not done yet

--- bootstrapMacOs.sh
+++ bootstrapMacOs.sh
@@ -3,53 +3,73 @@
 # Instalar gestor paquetes homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Instalar herramientas generales
-brew install \
-	fastfetch  \  # Herramienta para mostrar información del sistema
-	neovim  \     # Editor de texto avanzado
-	starship  \   # Prompt minimalista para la terminal
-	obsidian  \   # Herramienta de toma de notas
-	spotify  \    # Cliente de Spotify
-	android-studio  \  # IDE para desarrollo de Android
-	whisky  \     # Emulador de Windows para macOS
-	canva  \      # Herramienta de diseño gráfico
-	calibre  \    # Gestión de libros electrónicos
-	discord  \    # Plataforma de comunicación
-	megasync  # Cliente de sincronización para MEGA
+# Listas con descripciones
 
-# Instalar dependencias de desarrollo a nivel de sistema
-brew install \
-	bash  \  # Shell de Bash
-	git  \   # Sistema de control de versiones
-	make  \  # Herramienta de construcción
-	docker  \  # Plataforma de contenedores
-	docker-compose  \  # Herramienta para definir y ejecutar aplicaciones Docker multi-contenedor
-	colima  \  # Contenedores en macOS con Docker
-	stow  \  # Gestor de dotfiles
-	tldr  \  # Páginas de manual simplificadas
-	ripgrep  \  # Herramienta de búsqueda en línea de comandos
-	mactex-no-gui  # Distribución TeX para macOS sin GUI
+# Herramientas generales
+tools_general=(
+	"fastfetch  # Herramienta para mostrar información del sistema"
+	"neovim  # Editor de texto avanzado"
+	"starship  # Prompt minimalista para la terminal"
+	"obsidian  # Herramienta de toma de notas"
+	"spotify  # Cliente de Spotify"
+	"android-studio  # IDE para desarrollo de Android"
+	"whisky  # Emulador de Windows para macOS"
+	"canva  # Herramienta de diseño gráfico"
+	"calibre  # Gestión de libros electrónicos"
+	"discord  # Plataforma de comunicación"
+	"megasync  # Cliente de sincronización para MEGA"
+)
 
-# Instalar dependencias para lenguajes de programación (desarrollo y funcionamiento de nvim)
-brew install \
-	gcc  \  # Compilador para C y C++
-	lua  \  # Intérprete para Lua
-	luarocks  \  # Administrador de paquetes para Lua
-	python  \  # Intérprete para Python
-	openjdk  \  # Kit de desarrollo de Java
-	ruby  \  # Intérprete para Ruby
-	node  \  # Entorno para JavaScript
-	wget  \  # Herramienta de descarga
-	curl  \  # Herramienta de transferencia de datos
-	unzip  \  # Herramienta de descompresión
-	cmake  \  # Herramienta de generación de sistemas de construcción
-	ninja  \  # Herramienta de construcción de alto rendimiento
-	jq  \  # Procesador de JSON
-	clang-format  \  # Formateador de código para C/C++
-	rust  \  # Lenguaje de programación Rust
-	go  \  # Lenguaje de programación Go
-	php  \  # Lenguaje de programación PHP
-	composer  # Administrador de dependencias para PHP
+# Generar lista consolidada sin comentarios
+echo "Instalando paquetes generales"
+packages=$(for package in "${tools_general[@]}"; do echo "${package%%[[:space:]]*#*}"; done)
+brew install $packages
+
+# Dependencias de desarrollo a nivel de sistema
+dev_dependencies=(
+	"bash  # Shell de Bash"
+	"git  # Sistema de control de versiones"
+	"make  # Herramienta de construcción"
+	"docker  # Plataforma de contenedores"
+	"docker-compose  # Herramienta para definir y ejecutar aplicaciones Docker multi-contenedor"
+	"colima  # Contenedores en macOS con Docker"
+	"stow  # Gestor de dotfiles"
+	"tldr  # Páginas de manual simplificadas"
+	"ripgrep  # Herramienta de búsqueda en línea de comandos"
+	"mactex-no-gui  # Distribución TeX para macOS sin GUI"
+)
+
+echo "Instalando paquetes de desarrollo"
+packages=$(for package in "${dev_dependencies[@]}"; do echo "${package%%[[:space:]]*#*}"; done)
+brew install $packages
+
+
+# Dependencias para lenguajes de programación (desarrollo y funcionamiento de nvim)
+language_dependencies=(
+	"gcc  # Compilador para C y C++"
+	"lua  # Intérprete para Lua"
+	"luarocks  # Administrador de paquetes para Lua"
+	"python  # Intérprete para Python"
+	"openjdk  # Kit de desarrollo de Java"
+	"ruby  # Intérprete para Ruby"
+	"node  # Entorno para JavaScript"
+	"wget  # Herramienta de descarga"
+	"curl  # Herramienta de transferencia de datos"
+	"unzip  # Herramienta de descompresión"
+	"cmake  # Herramienta de generación de sistemas de construcción"
+	"ninja  # Herramienta de construcción de alto rendimiento"
+	"jq  # Procesador de JSON"
+	"rust  # Lenguaje de programación Rust"
+	"go  # Lenguaje de programación Go"
+	"php  # Lenguaje de programación PHP"
+	"composer  # Administrador de dependencias para PHP"
+)
+
+echo "Instalando paquetes de lenguaje y compilación"
+packages=$(for package in "${language_dependencies[@]}"; do echo "${package%%[[:space:]]*#*}"; done)
+brew install $packages
+
+
 # Poner los dotfiles en su sitio con gnu stow
 # Simulación del comando Stow
 echo "Simulando la aplicación de configuraciones con Stow:"
