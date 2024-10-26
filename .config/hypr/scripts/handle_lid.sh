2bd9d5c749c41cb1be808816b156df3cf43db6c2
Javier <javierperezsalasar@gmail.com>
2024-10-26 12:34:35 +0200

    Multi monitor support for clamshell to normal change

--- .config/hypr/scripts/handle_lid.sh
+++ .config/hypr/scripts/handle_lid.sh
@@ -4,8 +4,10 @@
 if [[ "$(hyprctl monitors)" == *"HDMI"* ]]; then
   if [[ $1 == "open" ]]; then
     # Al abrir la tapa, configura el monitor externo HDMI (1920x1080) arriba del interno eDP-1 (1920x1200)
-    #hyprctl keyword monitor "eDP-1,1920x1200,0,1080,1"   # Interno debajo
-	hyprctl keyword monitor "eDP-1,preferred,auto,1,bitdepth,10"
+	#hyprctl keyword monitor "HDMI-A-1,1920x1080@60,0,0,1,bitdepth,10"
+    #hyprctl keyword monitor "eDP-1,1920x1200@60,0,1080,1,bitdepth,10"
+	#Simplemente cargamos config de nuevo, sino error de sintaxis.
+	hyprctl reload
   else
     # Al cerrar la tapa, desactivar el monitor interno eDP-1
     hyprctl keyword monitor "eDP-1,disable"
