f0936cf29745583b4a656882fd88894b0cd20641
javips02 <javierperezsalasar@gmail.com>
2024-10-27 00:30:56 +0200

    Arch running config mods

--- bootstrapArchLinux.sh
+++ bootstrapArchLinux.sh
@@ -17,16 +17,17 @@ yay -Syu --devel
 yay -Y --devel --save
 
 #Greeter
-yay -S greetd-tuigreet-bin
-sudo systemctl enable 
+yay -S greetd-tuigreet
+sudo systemctl enable greetd
 #Hyprland ecosistem
-yay -S gnome-keyring qt5-wayland qt6-wayland mako xdg-desktop-portal-hyprland hyprshot waybar hypridle xwayland hyprpaper networkmanager qt5-base qt5-declarative qt6-base qt6-quickcontrols2 qt6-declarative
+yay -S hyprland gnome-keyring qt5-wayland qt6-wayland mako xdg-desktop-portal-hyprland hyprshot waybar hypridle xwayland hyprpaper networkmanager qt5-base qt5-declarative qt6-base qt6-quickcontrols2 qt6-declarative ttf-jetbrains-mono-nerd wofi gnome-themes-extra hyprlock
 #polkit
 yay -S hyprpolkitagent-git
 
 # Development Utils #
-yay -S jetbrains-toolbox
-
+yay -S jetbrains-toolbox visual-studio-code-bin qemu-full virt-manager libvirt
+sudo systemctl enable libvirtd
+sudo usermod -aG qemu libvirt javi
 # Flatpak section #
 yay -S flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -34,3 +35,9 @@ flatpak remote-add --if-not-exists --user launcher.moe https://gol.launcher.moe/
 flatpak install org.gnome.Platform//45
 flatpak install launcher.moe moe.launcher.the-honkers-railway-launcher
 flatpak install com.github.tchx84.Flatseal com.jgraph.drawio.desktop com.mattjakeman.ExtensionManager com.mudeprolinux.whakarere com.spotify.Client io.github.gamingdoom.Datcord md.obsidian.Obsidian org.gnome.seahorse.Application 
+
+# User packages
+# starship
+curl -sS https://starship.rs/install.sh | sh
+
+yay -S megasync-bin bashtop fastfetch nautilus
