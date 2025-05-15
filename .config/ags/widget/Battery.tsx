a6912e883c553619cda3b037c4bf1adc06bdf635
javips02 <javierperezsalasar@gmail.com>
2025-05-15 17:36:41 +0200

    Bat icon and calendar css in AGS

--- .config/ags/widget/Battery.tsx
+++ .config/ags/widget/Battery.tsx
@@ -1,20 +1,32 @@
 import Battery from "gi://AstalBattery"
 import { Gtk } from "astal/gtk4"
-import { bind } from "astal"
 
 export function Battery_icon() {
     const battery = Battery.get_default();
     
-    // Create a button with the battery percentage as label
-    const button = Gtk.Button.new_with_label(`BAT: ${battery.percentage*100}%`)
-    
-    // Define the callback function
-    const onButtonClicked = () => {
-        console.log(`Battery percentage: ${battery.percentage*100}%`)
-    }
-    
-    // Connect the clicked signal with the callback function
-    button.connect("clicked", onButtonClicked)
-    
-    return button
+    // Create widgets with icon and label
+    const button = Gtk.Button.new();
+    const box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 6);
+    const image = Gtk.Image.new_from_icon_name("battery-symbolic");
+    const label = Gtk.Label.new("Loading...");
+
+    box.append(image);
+    box.append(label);
+    button.set_child(box);
+
+    // Update battery status display
+    const updateBatteryStatus = () => {
+        const percentage = Math.round(battery.percentage * 100);
+        label.set_label(`${percentage}%`);
+        image.set_from_icon_name(battery.icon_name || "battery-symbolic");
+        console.log(`Battery icon name: ${battery.icon_name}`);
+    };
+
+    // Initial update
+    updateBatteryStatus();
+
+    // Click handler remains the same
+    button.connect("clicked", updateBatteryStatus);
+
+    return button;
 }
