4755466adaf7744f2925da175640f786b7ed5722
Javier <119667872+javips02@users.noreply.github.com>
2026-05-06 20:15:48 +0200

    fix: neovim config for treesitter

--- nvim/.config/nvim/lua/pluginsJavi.lua
+++ nvim/.config/nvim/lua/pluginsJavi.lua
@@ -89,8 +89,10 @@ require("lazy").setup({
 	},
 	{   -- Treesitter (parsers)
 		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.config").setup({
+			require("nvim-treesitter.configs").setup({
 				-- First 5 crucial to have
 				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 				auto_install = true, -- Automatically install missing parsers when entering buffer
